08eca8b277d9eef47f7bdeb36ec9dc010c14c794
imteyazm745 <imteyazm745@gmail.com>
2023-08-13 14:23:36 +0530

    solved

--- Hackerrank/Python/loop1.py
+++ Hackerrank/Python/loop1.py
@@ -9,3 +9,7 @@ The list of non-negative integers that are less than n=3 is [0,1,2]. Print the s
 1
 4
 """
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
